24b728aacb422598f163a45f961fb814eace16f5
adcpm <fabien@bonustrack.co>
2019-01-30 14:01:47 +0700

    Redirect with query string

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/*', (req, res) => {
-  res.redirect(`https://app.steemconnect.com${req.path}`);
+  res.redirect(`https://app.steemconnect.com${req.url}`);
 });
 
 module.exports = router;
