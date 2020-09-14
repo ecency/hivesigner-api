eeeba454a8b22c3d9a54eef2c00381ab35fe974c
feruz <fmiboy@gmail.com>
2020-09-14 11:51:47 +0300

    fix linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['airbnb-base'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'camelcase': 'off'
+    'camelcase': 'off',
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0,
   },
 };
