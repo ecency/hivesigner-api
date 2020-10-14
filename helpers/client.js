ccb24b2a3279a93946cc033c324a959a4677290b
feruz <fmiboy@gmail.com>
2020-10-14 22:54:32 +0300

    rebrand

--- helpers/client.js
+++ helpers/client.js
@@ -1,7 +1,5 @@
 import { Client } from '@hiveio/dhive';
 
-const client = new Client(process.env.BROADCAST_URL || 'https://api.hive.blog');
-
-client.updateOperations(true);
+const client = new Client(process.env.BROADCAST_URL || 'https://api.hive.blog', { rebrandedApi: true });
 
 export default client;
