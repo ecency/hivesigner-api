266e84c5e3016b7118e93625c4298381526429d3
feruz <fmiboy@gmail.com>
2020-10-14 22:49:03 +0300

    update operations

--- helpers/client.js
+++ helpers/client.js
@@ -1,11 +1,7 @@
 import { Client } from '@hiveio/dhive';
 
 const client = new Client(process.env.BROADCAST_URL || 'https://api.hive.blog');
-client.database.getVersion().then((res) => {
-  if (res.blockchain_version !== '0.23.0') {
-    // true: eclipse rebranded rpc nodes
-    // false: default old nodes (not necessary to call for old nodes)
-    client.updateOperations(true);
-  }
-});
+
+client.updateOperations(true);
+
 export default client;
