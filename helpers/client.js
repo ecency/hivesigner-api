6497fe6832c576e781efbe6be0c34b7968ddbcda
feruz <fmiboy@gmail.com>
2020-10-26 22:36:50 +0200

    update dhive and server list

--- helpers/client.js
+++ helpers/client.js
@@ -1,5 +1,16 @@
 import { Client } from '@hiveio/dhive';
 
-const client = new Client(process.env.BROADCAST_URL || 'https://api.hive.blog', { rebrandedApi: true });
+const DEFAULT_SERVER = [
+  'https://api.hive.blog',
+  'https://rpc.ecency.com',
+  'https://anyx.io',
+];
+
+const client = new Client([process.env.BROADCAST_URL || 'https://api.hive.blog', ...DEFAULT_SERVER], {
+  rebrandedApi: true,
+  timeout: 3000,
+  failoverThreshold: 15,
+  consoleOnFailover: true
+});
 
 export default client;
