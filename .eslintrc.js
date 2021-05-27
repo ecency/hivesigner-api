38ebedc7cf271f7dccc3b25dd5a2a948392c09b8
feruz <fmiboy@gmail.com>
2021-05-27 13:39:07 +0300

    keslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: ['airbnb-base'],
-  env: {
-    es6: true
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'camelcase': 'off',
