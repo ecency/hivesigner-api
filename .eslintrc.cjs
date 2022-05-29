d4b93e828758f0a8602eeb4b59f06d78f8cb2e9d
feruz <fmiboy@gmail.com>
2022-05-29 16:33:10 +0300

    eslint rule update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-  extends: ['airbnb-base'],
+  extends: ["airbnb-base"],
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
   },
   parser: "babel-eslint",
   env: { 
     "node": true 
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'camelcase': 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "camelcase": "off",
+    "import/prefer-default-export": "off",
     "no-underscore-dangle": 0,
     "no-param-reassign": 0,
   },
